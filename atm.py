d2e5033f7bc333922dabb3fe0245b4b880c84777
Ahmed <ahmadbilal971991@gmail.com>
2018-09-15 21:58:12 +0300

    Options for entering multiple amounts

--- atm.py
+++ atm.py
@@ -1,31 +1,47 @@
-account = 500
-request = 277
-def ATM(request,account):
-    if request <=0:
-        print "you must try with valid number "
-    elif account > request :
-            account -= request
-            while request > 0:
-                    if request>100 :
-                        print "give 100"
-                        request-=100
-                    elif request>50 :
-                        print "give 50"
-                        request-=50
-                    elif request>20 :
-                        print "give 20"
-                        request-=20
-                    elif request>10 :
-                        print "give 10"
-                        request-=10
-                    elif request>5 :
-                         print "give 5"
-                         request-=5
-                    else :
-                          print "give " , request
-                          request=0
-    print "New account = " +str(account )
+# allowed papers: 100, 50, 10, 5, and cents
 
+balance = 500
 
+request = 275
+def withdraw(balance, request):
 
-ATM (request,account )
+    if request > balance:
+        print("Can't give you all this money !!")
+
+    elif request < 0:
+        print("More than zero plz!")
+
+    else:
+        balance -= request
+        while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 20:
+                request -= 20
+                print("give 20")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+                request = 0
+                print "the new balance = " , balance
+                return balance
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
